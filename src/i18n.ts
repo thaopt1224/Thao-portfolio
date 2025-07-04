91c46ba13b2657f917f11979eb9d2dab608d8205
thaopt1224 <130853310+thaopt1224@users.noreply.github.com>
2025-07-04 11:44:03 +0900

    default en

--- src/i18n.ts
+++ src/i18n.ts
@@ -108,8 +108,8 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'vi',
-    fallbackLng: 'vi',
+    lng: 'en',
+    fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
     },
